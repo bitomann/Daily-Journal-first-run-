bfb60bc113beb4c41026bf9041bdbe7d6621760e
Bito Mann <bito@bitomann.com>
2019-10-18 15:47:17 -0500

    daily-journal-two

---
+++ src/scripts/journal.js
@@ -0,0 +1,16 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const NSSjournalEntry = {
+    date: "10/18/19",
+    concepts: "lab",
+    entry: "Feeling a little better today. Look forward to stepping away from code for the weekend and beginning groupProjects2 on Monday",
+    mood:"meh"
+}
+
+const journalEntries = [
+    
+];
+journalEntries.push(NSSjournalEntry);
+console.log(journalEntries)
